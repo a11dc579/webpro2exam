2b14aee86c2e506326ee7d91ebd7259dc4e5465f
a11dc579 <a11dc579@dhw.ac.jp>
2014-01-08 16:30:57 +0900

    indexページ
    商品一覧ページ作成

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>無題ドキュメント</title>
+</head>
+
+<body>
+<p>商品一覧</p><p>売上一覧</p>
+<p>&nbsp;</p>
+<h1>商品一覧</h1>
+<p>購入したい商品を選択してください</p>
+<ul>
+  <li>ふとんクリーナー</li>
+  <li>ダイソコードレスクリーナー</li>
+  <li>ロポット掃除機</li>
+  <li>ブラザーコンピュータミシン</li>
+  <li>毛玉取り器</li>
+  <li>ふとん専用ダニクリーナー</li>
+  <li>全自動洗濯機</li>
+  <li>コードレスアイロン</li>
+</ul>
+</body>
+</html>
